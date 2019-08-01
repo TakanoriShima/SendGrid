a8c7b7a94807035041bbb35a7a8386c2bac08c16
EC2 Default User <ec2-user@ip-172-31-47-19.us-east-2.compute.internal>
2019-08-01 09:06:45 +0000

    メッセージ変えた

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
+  default from: 'galaxy2quark@gmail.com'
   layout 'mailer'
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -106,3 +106,4 @@ Rails.application.configure do
     enable_starttls_auto: true
   }
 end
+
